0308581941860d2f7981b6fe33e7d57a7fbee857
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-11-23 01:15:20 +0500

    remove end slash

--- src/data/word-tag/word-tag.requests.js
+++ src/data/word-tag/word-tag.requests.js
@@ -1,7 +1,7 @@
 import request from "@/services/request";
 
 export const httpGetWordTags = () => {
-  return request.get("/word-tags/");
+  return request.get("/word-tags");
 };
 
 export const httpPostWordTag = (dto) => {
