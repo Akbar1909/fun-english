f6232c19ccd329eea66741e40ccdb0a4fd3b3017
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-11-23 18:43:39 +0500

    test

--- src/components/WordCard/WordCard.jsx
+++ src/components/WordCard/WordCard.jsx
@@ -41,6 +41,7 @@ export default function WordCard({
             className="text-center"
             width={300}
             height={300}
+            unoptimized
             src={`${process.env.NEXT_PUBLIC_STATIC_PATH}/${mediaName}`}
             alt="bird"
             priority
