a4da083f760ae1f342ce60d25369e3abcbf73eae
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-12-24 19:36:22 +0500

    fix error

--- src/middleware.js
+++ src/middleware.js
@@ -13,20 +13,18 @@ export default async function middleware(req) {
     //   },
     // };
     // const session = await getSession({ req: requestForNextAuth });
-
     // {
     //   session: {
     //   user: { name: 'target', email: 'tech.bobomurodov@gmail.com' },
     //   expires: '2024-01-23T12:36:24.212Z',
     //   accessToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFrYmFyIiwic3ViIjoxNSwiaWF0IjoxNzAzNDIxMjg5LCJleHAiOjE3MDQwMjYwODl9.NyEC4Ni55SY7Enwk90GRNkQP--HhjCtNZGJq6pdhqnU'
     // }
-
-    if (
-      !session?.accessToken &&
-      protectedRoutes.includes(req.nextUrl.pathname)
-    ) {
-      const absoluteURL = new URL("/?signin=false", req.nextUrl.origin);
-      return NextResponse.redirect(absoluteURL.toString());
-    }
+    // if (
+    //   !session?.accessToken &&
+    //   protectedRoutes.includes(req.nextUrl.pathname)
+    // ) {
+    //   const absoluteURL = new URL("/?signin=false", req.nextUrl.origin);
+    //   return NextResponse.redirect(absoluteURL.toString());
+    // }
   }
 }
