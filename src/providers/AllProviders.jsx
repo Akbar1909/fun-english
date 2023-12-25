a9d8423117fe38f9a3c2c2451f8c738bb2cee216
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-12-26 00:53:28 +0500

    make a sing component for SessionProvider

--- src/providers/AllProviders.jsx
+++ src/providers/AllProviders.jsx
@@ -2,12 +2,13 @@ import ReactQueryProvider from "./ReactQueryProvider";
 import ThemeProvider from "./ThemeProvider/ThemeProvider";
 import { ToastContainer } from "react-toastify";
 import { getServerSession } from "next-auth";
+import NextAuthProvider from "./NextAuthProvider";
 
 export const AllProviders = async ({ children }) => {
   return (
     <ReactQueryProvider>
       <ThemeProvider>
-        {children}
+        <NextAuthProvider>{children}</NextAuthProvider>
         <ToastContainer />
       </ThemeProvider>
     </ReactQueryProvider>
