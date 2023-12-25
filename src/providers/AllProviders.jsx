ade7594608f6a08a807121a6769c94fb792e4a3f
Akbar Bobomurodov <a.bobomurodov@expoplatform.com>
2023-12-25 16:17:37 +0500

    remove SessionProvider

--- src/providers/AllProviders.jsx
+++ src/providers/AllProviders.jsx
@@ -2,14 +2,12 @@ import ReactQueryProvider from "./ReactQueryProvider";
 import ThemeProvider from "./ThemeProvider/ThemeProvider";
 import { ToastContainer } from "react-toastify";
 import { getServerSession } from "next-auth";
-import SessionProvider from "./SessionProvider";
 
 export const AllProviders = async ({ children }) => {
-  const session = await getServerSession();
   return (
     <ReactQueryProvider>
       <ThemeProvider>
-        <SessionProvider session={session}>{children}</SessionProvider>
+        {children}
         <ToastContainer />
       </ThemeProvider>
     </ReactQueryProvider>
